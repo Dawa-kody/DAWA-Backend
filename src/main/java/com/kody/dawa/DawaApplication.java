913e1b2372d5814d12c326796a0a63860f7c460c
2rjswn <h47841930@gmail.com>
2024-09-05 19:43:17 +0900

    start

---
+++ src/main/java/com/kody/dawa/DawaApplication.java
@@ -0,0 +1,13 @@
+package com.kody.dawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawaApplication.class, args);
+	}
+
+}
