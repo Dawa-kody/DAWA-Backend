1e7a8358153cf60438ed6c82e9826c67b52cf303
2rjswn <h47841930@gmail.com>
2024-10-28 23:28:18 +0900

    edit deleteCode

--- src/main/java/com/kody/dawa/domain/auth/service/impl/PwChangeServiceImpl.java
+++ src/main/java/com/kody/dawa/domain/auth/service/impl/PwChangeServiceImpl.java
@@ -6,7 +6,6 @@ import com.kody.dawa.domain.auth.repository.AuthCodeRepository;
 import com.kody.dawa.domain.auth.service.PwChangeService;
 import com.kody.dawa.domain.user.entity.User;
 import com.kody.dawa.domain.user.repository.UserRepository;
-import com.kody.dawa.global.service.GetUser;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,7 +17,6 @@ import java.util.UUID;
 public class PwChangeServiceImpl implements PwChangeService {
     private final AuthCodeRepository authCodeRepository;
     private final UserRepository userRepository;
-    private final GetUser getUser;
     private final PasswordEncoder passwordEncoder;
     @Transactional
     public void execute(PwChangeRequest request) {
@@ -30,15 +28,12 @@ public class PwChangeServiceImpl implements PwChangeService {
 
         if (requestCode.equals(findCode.getCode())) {
             updatePassword(request.getPassword(), request.getEmail());
-            deleteByEmail(request.getEmail());
+            authCodeRepository.deleteByEmail(request.getEmail());
         } else {
             throw new RuntimeException("잘못된 인증 코드입니다.");
         }
     }
 
-    private void deleteByEmail(String email) {
-        authCodeRepository.deleteByEmail(email);
-    }
 
     private void updatePassword(String newPassword, String email) {
         User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("해당 이메일의 사용자가 존재하지 않습니다."));
