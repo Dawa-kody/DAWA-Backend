a518e8c7ae33a5421c02a303775fe41b963a7a35
2rjswn <h47841930@gmail.com>
2024-11-08 23:32:24 +0900

    edit repository

--- src/main/java/com/kody/dawa/domain/visit/repository/VisitRepository.java
+++ src/main/java/com/kody/dawa/domain/visit/repository/VisitRepository.java
@@ -1,3 +1,9 @@
 package com.kody.dawa.domain.visit.repository;
-public class VisitRepository {
+
+import com.kody.dawa.domain.visit.entity.Visit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VisitRepository extends JpaRepository<Visit, Long> {
 }
