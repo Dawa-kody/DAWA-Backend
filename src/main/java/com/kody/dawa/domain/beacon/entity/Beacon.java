7d47bea3f3e0314fe0c5d50b52af1aa84eb83680
2rjswn <h47841930@gmail.com>
2024-12-25 23:35:33 +0900

    edit beacon

--- src/main/java/com/kody/dawa/domain/beacon/entity/Beacon.java
+++ src/main/java/com/kody/dawa/domain/beacon/entity/Beacon.java
@@ -1,11 +1,35 @@
 package com.kody.dawa.domain.beacon.entity;
 
-import lombok.Data;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+@Table(name = "beacon")
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Beacon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String uuid;
     private int major;
     private int minor;
     private int rssi;
+    private boolean isHere;
+
+    public void setIsHere(boolean isHere) {
+        this.isHere = isHere;
+    }
+
+    public void setRssi(int rssi) {
+        this.rssi = rssi;
+    }
 }
