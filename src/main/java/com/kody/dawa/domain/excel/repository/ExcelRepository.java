386dadd2f2c60425168e06c40c85c9e3e7c1ebb1
minsung gwak <131960511+opinionist@users.noreply.github.com>
2025-03-04 12:24:17 +0900

    excel화 하기 4. 환경변수 설정

--- src/main/java/com/kody/dawa/domain/excel/repository/ExcelRepository.java
+++ src/main/java/com/kody/dawa/domain/excel/repository/ExcelRepository.java
@@ -1,9 +1,10 @@
-//package com.kody.dawa.domain.excel.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface ExcelRepository extends JpaRepository {
-//
-//}
+package com.kody.dawa.domain.excel.repository;
+
+import com.kody.dawa.domain.questionnaire.entity.Questionnaire;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExcelRepository extends JpaRepository<Questionnaire,Long> {
+
+}
--- src/main/java/com/kody/dawa/domain/excel/service/ExcelService.java
+++ src/main/java/com/kody/dawa/domain/excel/service/ExcelService.java
@@ -1,11 +1,11 @@
-//package com.kody.dawa.domain.excel.service;
-//
-//import com.kody.dawa.domain.excel.presentation.dto.request.ExcelDateRequest;
-//import com.kody.dawa.domain.excel.presentation.dto.request.ExcelStudentRequest;
-//import org.apache.poi.ss.usermodel.Sheet;
-//
-//public interface ExcelService {
-//    void createXssStudent(ExcelStudentRequest request, Sheet sheet);
-//    void createXssMonth(ExcelDateRequest request, Sheet sheet);
-//    void createXssYear(ExcelDateRequest request, Sheet sheet);
-//}
+package com.kody.dawa.domain.excel.service;
+
+import com.kody.dawa.domain.excel.presentation.dto.request.ExcelDateRequest;
+import com.kody.dawa.domain.excel.presentation.dto.request.ExcelStudentRequest;
+import org.apache.poi.ss.usermodel.Sheet;
+
+public interface ExcelService {
+    void createXssStudent(ExcelStudentRequest request, Sheet sheet);
+    void createXssMonth(ExcelDateRequest request, Sheet sheet);
+    void createXssYear(ExcelDateRequest request, Sheet sheet);
+}
