95461bb1efa27f961389d66d46b7c07ec830040e
2rjswn <h47841930@gmail.com>
2025-04-16 23:34:05 +0900

    edit user

---
+++ src/main/java/com/kody/dawa/domain/user/enums/Gender.java
@@ -0,0 +1,11 @@
+package com.kody.dawa.domain.user.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum Gender {
+    MAN("남성"),
+    WOMAN("여성");
+
+    private final String name;
+}
