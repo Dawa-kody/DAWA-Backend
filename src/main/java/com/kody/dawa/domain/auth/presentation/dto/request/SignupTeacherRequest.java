85b017738cf6db40a56704c1ce9d73a09313df3b
2rjswn <h47841930@gmail.com>
2025-04-23 08:51:52 +0900

    add signupTeacher

--- src/main/java/com/kody/dawa/domain/auth/presentation/dto/request/SignupTeacherRequest.java
+++ src/main/java/com/kody/dawa/domain/auth/presentation/dto/request/SignupTeacherRequest.java
@@ -1,4 +1,33 @@
 package com.kody.dawa.domain.auth.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kody.dawa.domain.user.enums.Gender;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignupTeacherRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @NotBlank
+    private Gender gender;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String department;
 }
--- src/main/java/com/kody/dawa/domain/auth/service/impl/signupTeacherServiceImpl.java
+++ src/main/java/com/kody/dawa/domain/auth/service/impl/signupTeacherServiceImpl.java
@@ -1,4 +1,41 @@
 package com.kody.dawa.domain.auth.service.impl;
 
+import com.kody.dawa.domain.auth.presentation.dto.request.SignupTeacherRequest;
+import com.kody.dawa.domain.user.entity.User;
+import com.kody.dawa.domain.user.enums.Role;
+import com.kody.dawa.domain.user.repository.UserRepository;
+import com.kody.dawa.global.exception.HttpException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class signupTeacherServiceImpl {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    @Transactional
+    public void signupTeacher(SignupTeacherRequest request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new HttpException(HttpStatus.NOT_FOUND, "없는 유저 입니다."));
+
+        if (user.getPassword() != null || user.getName() != null) {
+            throw new HttpException(HttpStatus.BAD_REQUEST, "이미 회원가입을 완료한 유저입니다.");
+        }
+
+        if(!user.isEmailVerifyStatus()) {
+            throw new HttpException(HttpStatus.BAD_REQUEST, "인증되지 않은 유저입니다");
+        }
+
+        user.setName(request.getName());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setDepartment(request.getDepartment());
+        user.setRoles(List.of(Role.ROLE_USER));
+        user.setGender(request.getGender());
+        userRepository.save(user);
+    }
 }
