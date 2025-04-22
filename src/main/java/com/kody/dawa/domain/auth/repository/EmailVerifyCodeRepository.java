0b7ec3d74ffe793eddc44ce9c11ba2ad02db0ef0
2rjswn <h47841930@gmail.com>
2025-04-23 00:26:47 +0900

    edit EmailVerifyCodeRepository

--- src/main/java/com/kody/dawa/domain/auth/repository/EmailVerifyCodeRepository.java
+++ src/main/java/com/kody/dawa/domain/auth/repository/EmailVerifyCodeRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface EmailVerifyCodeRepository extends JpaRepository<EmailVerifyCode, UUID> {
+    EmailVerifyCodeRepository findByEmail(String email);
+    void deleteByEmail(String email);
 }
