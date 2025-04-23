86894b99de400de3649d7f1e9d6960c9c7ddbe5e
2rjswn <h47841930@gmail.com>
2025-04-23 10:19:34 +0900

    add PasswordChangeCodeRequest

---
+++ src/main/java/com/kody/dawa/domain/auth/presentation/dto/request/PasswordChangeCodeRequest.java
@@ -0,0 +1,14 @@
+package com.kody.dawa.domain.auth.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordChangeCodeRequest {
+    @NotBlank
+    private String email;
+}
