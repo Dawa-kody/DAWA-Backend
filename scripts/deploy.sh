9da0f6afa91ed68ba49ae2dab763342b59d7d223
이건주 <131886304+2rjswn@users.noreply.github.com>
2025-09-09 22:53:52 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
+set -e
+
+cd ~/DAWA-Backend
+
+git reset --hard
 git pull origin main
 
 chmod +x ./gradlew
-
 ./gradlew clean build
 
-docker build . -t dawa/dawa-server:latest
+docker build -t dawa/dawa-server:latest .
 
-docker stop dawa-server
-docker rm dawa-server
+docker stop dawa-server || true
+docker rm dawa-server || true
 
 docker run -d -p 8080:8080 --name dawa-server --env-file ./.env dawa/dawa-server:latest
